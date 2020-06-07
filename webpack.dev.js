739f826b0fbe1d243e12399aded7e03a23ffaad6
dreyes <dreyesb5@gmail.com>
2020-06-06 21:40:59 -0600

    feat: Sass added and configured in webpack

--- webpack.dev.js
+++ webpack.dev.js
@@ -8,11 +8,15 @@ module.exports = {
 	entry: './src/client/index.js',
 	module: {
 		rules: [
-						{
+			{
 				test: '/\.js$/',
 				exclude: /node_modules/,
 				loader: "babel-loader"
-						}
+      },
+      {
+        test: /\.scss$/,
+        use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+      }
 		]
 	},
 	plugins: [
