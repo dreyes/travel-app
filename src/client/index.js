ac11c2a7d658e6644d740ca8d177dcc2ddcb5107
dreyes <dreyesb5@gmail.com>
2020-06-12 13:24:15 -0600

    feat: EventListener added to button

--- src/client/index.js
+++ src/client/index.js
@@ -2,6 +2,9 @@ import { testFunction } from './js/app'
 import './styles/style.scss'
 
 console.log('hello webpack');
+document.getElementById('test-btn').addEventListener('click', () => {
+  testFunction();
+});
 
 export {
   testFunction
