a660eae8c2fdb2a46f4f4e975acfb9754e6db389
dreyes <dreyesb5@gmail.com>
2020-06-12 09:26:35 -0600

    feat: dotenv added

--- src/server/server.js
+++ src/server/server.js
@@ -1,7 +1,9 @@
 // Express and dependencies
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
 
 // Start up an instance of app
 const app = express();
